d320b448ec76a5b9e6a4a60c4b53f11c1fc43311
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-10-17 17:02:41 +0530

    Add files via upload

---
+++ class_1.py
@@ -0,0 +1,22 @@
+print("Hello world..")
+#comments
+"""nrcfjrbfvf
+cbdjcbdvf
+bhjvbfv"""
+
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.""")
+
+x = 5
+y = 10
+print("Sum of",x,"and",y,"is:",x+y)
+print("div of %d and %d is: %f"%(x,y,x/y))
+print("Div of {} and {} is {}".format(x,y,x/y))
+print(f"Div of {x} and {y} is: {x/y}")
