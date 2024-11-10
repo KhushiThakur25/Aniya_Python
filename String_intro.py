b5e7744915bfab980e25fee070eee4dc7e01a4da
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-11-10 16:34:50 +0530

    Add files via upload

---
+++ String_intro.py
@@ -0,0 +1,36 @@
+st = "This is python code.."
+# This is collection of characters..
+strs = 'This is python language..'
+
+print(type(st))
+print(type(strs))
+
+print(st[3])
+# st[3] = 'y'
+print(st.title())
+st = st.upper()
+print(st)
+
+print(st.find('z'))
+
+# 1.upper()
+# 2.lower()
+# 3.capitalize()
+# 4.title()
+# 5.casefold()
+# 6.isupper()
+# 7.islower()
+# 8.strip()
+# 9.lstrip()
+# 10.rstrip()
+# 11.partition()
+# 12.join()
+# 13.replace()
+# 14.index()
+# 15.rindex()
+# 16.find()
+# 17.count()
+# 18.len()
+# 19.type()
+# 20.split()
+#21.swapcase()
